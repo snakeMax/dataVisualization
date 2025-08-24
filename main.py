b8f71a0adeb202ebb73957f736126e0f494e4b0c
snakeMax <38764043+snakeMax@users.noreply.github.com>
2025-08-24 12:28:48 +0200

    added second graph

--- main.py
+++ main.py
@@ -4,15 +4,21 @@ import pandas as pd
 
 #### Basic Graphs
 
-x = np.array([1, 2, 3, 4, 16])
-y = np.array([1, 4, 9, 16, 50])
 
+X = np.array([1, 2, 3, 4, 16])
+Y = np.array([2, 6, 8, 14, 32])
 
-plt.plot(x, y)
+x = np.arange(5, 15) ## create linear array
+y = x ** 2 ## create quadratic array
+
+plt.plot(x, y, label="y = x^2", color="green", linestyle="--", marker="o", markersize=10, markerfacecolor="blue", markeredgecolor="red")
+plt.plot(X, Y, label="line 2", color="red", linestyle="-", marker="o", markersize=10, markerfacecolor="red")
 
 plt.title("Basic Graph", fontdict={"fontname": "Comic Sans MS"}, fontsize=20)
 
 plt.xlabel("X-axis", fontsize=20)
 plt.ylabel("Y-axis", fontsize=20)
+
+plt.legend()
 plt.show()
 
